2f7d00d2a994e2deb74994bd5e986ac25931e112
Faza <firdausauliafaza@gmail.com>
2021-05-16 17:59:06 +0700

    niomic.com

---
+++ tugas15.js
@@ -0,0 +1,7 @@
+let ab = 8;
+let bc = 6;
+let ac = Math.sqrt(Math.pow(ab, 2) + Math.pow(bc, 2));
+
+console.log("Nilai AB : ", ab);
+console.log("Nilai BC : ", bc);
+console.log("Panjang sisi AC : ", ac);
